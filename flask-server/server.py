73bb1811cb01eafcc454bc3c6fc43886c274cded
Aidan <abayercalvert@gmail.com>
2024-02-01 20:32:20 -0800

    inital setup, Flask and React initialized, flask API working

---
+++ flask-server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Members API route
+@app.route('/members')
+def member():
+    return {"members": ['member1', 'member2', 'member3']}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
